227f0c9554acbf32545f609306328c1b809fc1ae
FranckPedron <franckyp1+rapports_git@gmail.com>
2024-05-30 16:29:58 +0200

    Ajout de la création et récupération des tâches

--- app/page.tsx
+++ app/page.tsx
@@ -3,17 +3,71 @@
 import {useState, useEffect} from "react";
 import Header from "@/components/Header";
 import AddTask from "@components/AddTask";
+import {ITask} from "@types";
+import {Flex, Spinner} from "@chakra-ui/react";
+import NoTask from "@components/NoTask";
 
 
 export default function Home() {
 
     const [task, setTask] = useState("");
-    const handleCreateTask = async() => {}
+    const [isLoading, setIsLoading] = useState(true);
+    const [allTasks, setAllTasks] = useState([]);
 
+    const handleCreateTask = async () => {
+        setIsLoading(true);
+        try {
+            const response = await fetch("/api/task/new", {
+                method: "POST",
+                body: JSON.stringify({
+                    task: task,
+                }),
+            })
+            if (response.ok) {
+                setTask("");
+                fetchTasks();
+            } else {
+                console.error("Error creating task");
+            }
+        } catch (error) {
+            console.log(error);
+        }
+        setIsLoading(false);
+    }
+
+    const fetchTasks = async () => {
+        try {
+            const response = await fetch("/api/task/all");
+            const data = await response.json();
+            setAllTasks(data);
+            setIsLoading(false);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    useEffect(() => {
+        fetchTasks();
+    }, [])
     return (
         <>
-            <Header />
+            <Header/>
             <AddTask task={task} setTask={setTask} handleCreateTask={handleCreateTask}/>
+            {isLoading ? (
+                <Spinner/>
+            ) : (
+                <>
+                    <Flex p="2rem" direction="column">
+                        {allTasks.length > 0 ?
+                            allTasks.map((iTask: ITask) => (
+                                <p>{iTask.task}</p>
+                            )) : (
+                                <NoTask/>
+                            )
+                        }
+                    </Flex>
+                </>
+            )}
         </>
     );
 }
--- components/AddTask.tsx
+++ components/AddTask.tsx
@@ -4,8 +4,6 @@ import {SmallAddIcon} from "@chakra-ui/icons";
 import {AddTaskProps} from '@types';
 
 const AddTask = ({task, setTask, handleCreateTask}: AddTaskProps) => {
-
-
     return (
         <Flex pt="2rem" pl="2rem" pr="2rem" pb="1rem">
             <Input
@@ -25,4 +23,5 @@ const AddTask = ({task, setTask, handleCreateTask}: AddTaskProps) => {
         </Flex>
     )
 }
+
 export default AddTask
